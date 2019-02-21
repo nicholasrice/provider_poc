81fcf19db2a2e954fb1883fc3d087731fabd9612
Shaun Campbell <shcampbe@microsoft.com>
2019-02-21 09:41:45 -0800

    Add multi root, add fake sheet mgr

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -1,34 +1,48 @@
 import React, { Component, Children } from "react";
 import "./provider.css"
 
-interface SpecifyProps {
-    depth: number,
-    id?: number;
+function generateId(length: number = 5): string {
+    return `${Math.round(Math.random() * Math.pow(10, length))}`.padStart(length, "0");
 }
 
-const SpecifyContext = React.createContext<SpecifyProps>({depth: 0, id: undefined});
+class SheetManager {
+    private _id = generateId();
+    id() {
+        return `Sheet Manager ${this._id}`;
+    }
+}
+
+interface SpecifyProps {
+    sheetManager: SheetManager
+    depth: number,
+    id?: string;
+}
+
+const SpecifyContext = React.createContext<SpecifyProps>({sheetManager: new SheetManager(), depth: 0, id: undefined});
 
 
 export class ProviderBase extends Component<SpecifyProps> {
     render() {
+        const currentContext = this.props;
         const context: SpecifyProps = {
-            depth: this.props.depth + 1,
-            id: this.props.id ? this.props.id : this.generateId()
+            sheetManager: currentContext.sheetManager,
+            depth: currentContext.depth + 1,
+            id: currentContext.id ? (`${currentContext.id}-${generateId(2)}`) : generateId(5)
         };
         return (
             <SpecifyContext.Provider value={context}>
                 <div className={"provider"}>
-                    Depth: {context.depth} Id: {context.id}
+                    Depth: {context.depth} 
+                    <br/>
+                    Id: {context.id} 
+                    <br/>
+                    {context.sheetManager.id()}
                     <br/>
                     { this.props.children }
                 </div>
             </SpecifyContext.Provider>
         )
-    }
-
-    private generateId(): number {
-        return Math.round(Math.random() * 10000);
-    }
+    } 
 }
 
 export const SpecifyProvider: React.FC = (props) => {
